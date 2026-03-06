d84fb9079a2d00bdd8b072d9630885c662bc3817
Botir Ismatov <botirismatov@Botir.local>
2026-03-07 03:14:14 +0500

    fdsfsdf sdf sadf

--- routes/client.php
+++ routes/client.php
@@ -7,13 +7,15 @@ use App\Http\Controllers\Client\OrderController;
 use App\Http\Controllers\Client\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'client'])
+Route::middleware([
+    //'auth', 'client'
+])
     ->prefix('client')
     ->name('client.')
     ->group(function (): void {
         Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-        Route::resource('items', ItemController::class);
-        Route::resource('orders', OrderController::class);
-        Route::resource('marks', MarkController::class);
-        Route::resource('users', UserController::class);
+        Route::resource('item', ItemController::class);
+        Route::resource('order', OrderController::class);
+        Route::resource('mark', MarkController::class);
+        Route::resource('user', UserController::class);
     });
